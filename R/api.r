7ad1e5c3e92d173d65b5173695f3c44d14d4ff8f
Benrath <meulemann@googlemail.com>
2023-10-09 10:49:19 +0200

    Update api.r
    Piping entfernt

--- R/api.r
+++ R/api.r
@@ -1,13 +1,11 @@
-#' Make a request tot the fahrplan endpoint
-#' @noRd
+# Make a request tot the fahrplan endpoint
+# @noRd
 get_results <- function(req_body) {
-  # TODO: remove progress bar for req_throttle
-  httr2::request("https://www.bahn.de/web/api/angebote/fahrplan") |>
-    httr2::req_headers(
-      "user-agent" = "bahnr"
-    ) |>
-    httr2::req_body_json(req_body) |>
-    httr2::req_throttle(5 / 60) |>
-    httr2::req_perform() |>
-    httr2::resp_body_json()
+  response <- httr2::request("https://www.bahn.de/web/api/angebote/fahrplan")
+  response <- httr2::req_headers(response, "user-agent" = "bahnr")
+  response <- httr2::req_body_json(response, req_body)
+  response <- httr2::req_throttle(response, 5 / 60)
+  response <- httr2::req_perform(response)
+  result <- httr2::resp_body_json(response)
+  return(result)
 }
